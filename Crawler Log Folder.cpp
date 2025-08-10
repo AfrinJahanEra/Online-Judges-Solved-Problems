a853942931e117f8e418fa152eea6ef0188d408d
era97 <afrinjahanera03@gmail.com>
2025-08-11 02:37:20 +0600

    Stack problem solved

---
+++ Crawler Log Folder.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int depth = 0;
+        for (auto &log : logs) {
+            if (log == "../") {
+                if (depth > 0) depth--;
+            } else if (log == "./") {
+              
+            } else {
+                depth++; 
+            }
+        }
+        return depth;
+    }
+};
---
+++ Make The String Great.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <cctype>
+using namespace std;
+
+class Solution {
+public:
+    string makeGood(string s) {
+        string result;
+        for (char c : s) {
+            if (!result.empty() && abs(result.back() - c) == 32) {
+               
+                result.pop_back();
+            } else {
+                result.push_back(c);
+            }
+        }
+        return result;
+    }
+};
