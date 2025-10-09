5817e6f48693f283e73c9899db17ac4ec12968d3
era97 <afrinjahanera03@gmail.com>
2025-10-09 20:59:46 +0600

    cf mahbub mex problems

---
+++ A_Mahmoud_and_Ehab_and_the_MEX.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,x;
+    cin>>n>>x;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int count =0;
+    if(find(a.begin(),a.end(),x)!=a.end()){
+        count++;
+        
+    }
+    for(int i=0;i<x;i++){
+        if(find(a.begin(),a.end(),i)==a.end()){
+            
+            count++;
+        }
+        
+    }
+    cout<<count<<endl;
+    return 0;
+}
