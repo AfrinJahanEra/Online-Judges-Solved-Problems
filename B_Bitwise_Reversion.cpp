a050e8481a3f858dfdebce4808054b46b2ab7ae9
era97 <afrinjahanera03@gmail.com>
2025-10-10 23:16:58 +0600

    Div 2 B

---
+++ B_Bitwise_Reversion.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        ll x,y,z;
+        cin>>x>>y>>z;
+        if((x&y)==(y&z)&&(y&z)==(z&x)){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
+    }
+    return 0;
+}
