c01ac67abb92d88b47be30b4e6c42c7e02d055d5
era97 <afrinjahanera03@gmail.com>
2025-10-21 05:24:26 +0600

    push

---
+++ C_Update Queries.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        string s;
+        cin>>s;
+        vector<int> a(m);
+        for(int i=0;i<m;i++){
+            cin>>a[i];
+        }
+        string c;
+        cin>>c;
+        sort(a.begin(),a.end());
+        a.erase(unique(a.begin(),a.end()),a.end());
+        sort(c.begin(),c.end());
+        int k=a.size();
+        for(int i=0;i<k;i++){
+            s[a[i]-1]=c[i];
+        }
+        cout<<s<<endl;
+    }
+    return 0;
+}
