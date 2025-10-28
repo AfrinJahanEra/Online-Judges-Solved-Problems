e4faa5d5d5048efff0267c5e6416d9dfc1f1cbb6
era97 <afrinjahanera03@gmail.com>
2025-10-28 12:30:38 +0600

    Push

---
+++ L_Integer_Average.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        int e=0,o=0;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            if(a[i]%2==0){
+                e++;
+            }else{
+                o++;
+            }
+        }
+        if(n<2){
+            cout<<"NO"<<endl;
+            continue;
+        }
+        if(e>=2||o>=2){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
