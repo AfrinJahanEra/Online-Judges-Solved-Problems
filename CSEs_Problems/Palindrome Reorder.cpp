353a3f38ff8e6ea089d95346156d07131ad7bbaa
era97 <afrinjahanera03@gmail.com>
2025-11-17 08:49:17 +0600

    Palindrome Reorder

---
+++ CSEs_Problems/Palindrome Reorder.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s; 
+    cin>>s;
+    vector<long long> f(26, 0);
+    for(char ch:s){
+        f[ch-'A']++;
+    }
+    int o=0;
+    int id=-1;
+    for(int i=0;i<26;i++){
+        if(f[i]%2){
+            o++;
+            id=i;
+        }
+    }
+    if(o>1){
+        cout<<"NO SOLUTION";
+        return 0; 
+    }
+    string a;
+    a.reserve(s.size()/2);
+    for(int i=0;i<26;i++){
+        a.append(f[i]/2,'A'+i);
+    }
+    string b;
+    if(id!=-1){
+        b.append(f[id]%2,'A'+id);
+    }
+    cout<<a;
+    if(!b.empty()){
+        cout<<b;
+    }
+    reverse(a.begin(),a.end());
+    cout<<a;
+    return 0;
